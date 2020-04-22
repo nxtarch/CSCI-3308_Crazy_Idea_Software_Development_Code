1c5704bf36dbcf0cc3bb64ab2673c1afb9bc6f5f
mtefridj <malik.tefridj@gmail.com>
2020-04-21 23:03:24 -0600

    fixed server code and added style sheet for searchpage

--- CSCI_Node.js
+++ CSCI_Node.js
@@ -3,7 +3,9 @@
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
+var path = require('path');
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/');
 app.use(express.static(__dirname + '/'));
 // DB credentials might have to change in the future
 var con = mysql.createConnection({
@@ -32,7 +34,11 @@ con.connect(function(err){
 });
 // random meal selector
 
-app.get('/', function(req,res){
-	res.send(dataVar);
+app.get('/search_page', function(req,res){
+	res.sendFile(path.join(__dirname + '/search_page.html'));
 });
-app.listen(8000);
+app.post('/search_page', function(req, res){
+	var user_Input = req.body.searchBar;
+	console.log(user_Input);
+});
+app.listen(3000);
